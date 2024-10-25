538a6d1c27c8c86addf7f9ce6ba316285755ce1a
Israel Adekanmi <adekanmiisrael8@gmail.com>
2024-10-25 19:23:30 +0100

    Express.js Class

--- backend/Haatchdev Express/src/main.js
+++ backend/Haatchdev Express/src/main.js
@@ -1,11 +1,30 @@
 import express from "express";
+import { users } from "./users.js";
 
 const app = express();
 
-app.get("/greet", (req, res) => {
-  res.send("Hello World!");
+app.get("/greet/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello ${name}, Wassup bro?`);
+  //   res.json({ message: "Hello World!" });
 });
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001')
-})
+app.get("/users", (req, res) => {
+  res.json({ users });
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const user = users.find((user) => user.id === parseInt(id));
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: "User not found" }); // Handle user not found
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3001");
+});
---
+++ backend/Haatchdev Express/src/users.js
@@ -0,0 +1,6 @@
+export let users = [
+    { id: 1, name: 'John', age: 24},
+    { id: 2, name: 'David', age: 14},
+    { id: 3, name: 'Jess', age: 27},
+    { id: 4, name: 'Alfred', age: 20},
+];
